32b3698e88c7efe07b5b24eb9f035048e302de2b
Ryan Fitzgerald <ryfimedia@gmail.com>
2025-07-09 19:06:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/templates/page.tsx
+++ src/app/(app)/templates/page.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardFooter, CardDescription } from '@/components/ui/card';
 import { Eye, PlusCircle } from 'lucide-react';
 import { useToast } from "@/hooks/use-toast";
 import {
